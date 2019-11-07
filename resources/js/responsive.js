7b3c933373ba885708e41a481b7ded374afff0e2
ngvuthanhnhan <56341874+ngvuthanhnhan@users.noreply.github.com>
2019-11-07 18:40:58 +0700

    Updated header and unresolved error

--- resources/js/responsive.js
+++ resources/js/responsive.js
@@ -2,3 +2,14 @@ function headerTogger() {
   var x = document.getElementById('header');
   x.classList.toggle('responsive');
 }
+
+window.onscroll = function() { this.stickyHeader() };
+var header = document.getElementById("header");
+function stickyHeader() {
+  if(window.pageYOffset > 0) {
+    header.classList.add("sticky-header");
+  }
+  else {
+    header.classList.remove("sticky-header");
+  }
+}
