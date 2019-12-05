6d783ae69d2ab72c62c6da25ff33be55c4f94c6a
ngvuthanhnhan <56341874+ngvuthanhnhan@users.noreply.github.com>
2019-12-05 16:44:47 +0700

    Build Github overview block
    - update github.js
    - make some changes at box-shadow
    - remove "see more" button

---
+++ resources/js/github.js
@@ -0,0 +1,37 @@
+const dummyGithub = [
+    {
+        title: 'Lorem ipsum',
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime voluptatibus eveniet, repudiandae excepturi ducimus animi omnis rerum?',
+    },
+    {
+        title: 'Lorem ipsum',
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime voluptatibus eveniet, repudiandae excepturi ducimus animi omnis rerum?',
+    },
+    {
+        title: 'Lorem ipsum',
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime voluptatibus eveniet, repudiandae excepturi ducimus animi omnis rerum?',
+    }
+];
+
+var cardlist = document.getElementsByClassName('card-list');
+
+for (var i = 0; i < dummyGithub.length; i++){
+    var title = document.createElement('DIV');
+    title.classList.add('git-title');
+    title.appendChild(document.createTextNode(dummyGithub[i].title));
+
+    var descrp = document.createElement('DIV');
+    descrp.classList.add('git-descrp');
+    descrp.appendChild(document.createTextNode(dummyGithub[i].description));
+
+    var gcard = document.createElement('DIV');
+    gcard.classList.add('card');
+    gcard.appendChild(title)
+    gcard.appendChild(descrp);
+
+    var container = document.createElement('DIV');
+    container.classList.add('card-container');
+    container.appendChild(gcard);
+
+    cardlist[1].appendChild(container);
+}
